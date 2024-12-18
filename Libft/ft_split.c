6b7d965e535df69b0ad81ce58dde552241b219bb
hilda0000FF <m.sarrico.tice@outlook.com>
2024-12-18 13:05:14 +0000

    Add files via upload

---
+++ Libft/ft_split.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtice <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/09 15:46:53 by mtice             #+#    #+#             */
+/*   Updated: 2024/12/13 14:31:45 by mtice            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_string_counter(char const *s, char c)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (s[i] != '\0')
+	{
+		while (s[i] == c && s[i] != '\0')
+			i++;
+		if (s[i] != c && s[i] != '\0')
+			j++;
+		while (s[i] != c && s[i] != '\0')
+			i++;
+	}
+	return (j);
+}
+
+static char	*ft_string_in_pointer(char const *s, char c, int i, int len)
+{
+	char	*p;
+	int		j;
+
+	p = (char *)malloc(sizeof(char) * (len + 1));
+	if (p == NULL)
+		return (NULL);
+	j = 0;
+	while (s[i - len] != c && len - 1 >= 0)
+	{
+		p[j] = s[i - len];
+		len --;
+		j++;
+	}
+	p[j] = '\0';
+	return (p);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int		i;
+	int		k;
+	int		len;
+	char	**arr;
+
+	i = 0;
+	k = 0;
+	arr = (char **)malloc(sizeof(char *) * (ft_string_counter(s, c) + 1));
+	if (arr == NULL)
+		return (NULL);
+	while (s[i] != '\0' && k < ft_string_counter(s, c))
+	{
+		while (s[i] == c && s[i] != '\0')
+			i++;
+		len = 0;
+		while (s[i] != c && s[i] != '\0')
+		{
+			len++;
+			i++;
+		}
+		arr[k] = ft_string_in_pointer(s, c, i, len);
+		k++;
+	}
+	arr[k] = NULL;
+	return (arr);
+}
+/*
+int	main(void)
+{
+	char str[] = "";
+	char del = ' ';
+	char **result;
+	int	j;
+
+	result = ft_split(str, del);
+	j = 0;
+	while (j < ft_string_counter(str, del))
+	{
+		printf("%s\n", result[j]);
+		j++;
+	}
+	free (result);
+	return (0);
+}*/
---
+++ Libft/ft_strmapi.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtice <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/11 14:06:10 by mtice             #+#    #+#             */
+/*   Updated: 2024/12/11 16:42:47 by mtice            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlen(const char *s);
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*p;
+
+	i = 0;
+	p = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (p == NULL)
+		return (NULL);
+	while (i < ft_strlen(s))
+	{
+		p[i] = f(i, s[i]);
+		i++;
+	}
+	p[i] = '\0';
+	return (p);
+}
+/*
+int main(void)
+{
+	char	str[] = "Hello";
+	char	*result;
+	unsigned int	i;
+	
+	i = 0;
+	result = ft_strmapi(str, ft_upper);
+	printf("%s\n", result);
+	free(result);
+	return (0);
+}*/
